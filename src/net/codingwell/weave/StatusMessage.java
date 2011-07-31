6ba40501dfc6a8e9feb3517e5d54d585f863e547
tsuckow <tsuckow@gmail.com>
2011-07-31 14:46:11 -0700

    License Stuff

--- src/net/codingwell/weave/StatusMessage.java
+++ src/net/codingwell/weave/StatusMessage.java
@@ -1,10 +1,8 @@
-/*
-Copyright 2010 Coding Well
-All rights reserved. This program and the accompanying materials 
-are made available under the terms of the Eclipse Public License v1.0 
-which accompanies this distribution, and is available at 
-http://www.eclipse.org/legal/epl-v10.html
- */
+// Copyright (c) 2010 Thomas Suckow
+// All rights reserved. This program and the accompanying materials 
+// are made available under the terms of the Eclipse Public License v1.0 
+// which accompanies this distribution, and is available at 
+// http://www.eclipse.org/legal/epl-v10.html
 
 package net.codingwell.weave;
 
