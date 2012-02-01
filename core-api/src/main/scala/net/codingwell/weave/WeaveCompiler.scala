3c1b78e06b2e7d3acbd008d7f7d780ae83794744
Thomas <tsuckow@gmail.com>
2012-02-01 00:32:38 -0800

    Work on an actor for the compiler

--- core-api/src/main/scala/net/codingwell/weave/WeaveCompiler.scala
+++ core-api/src/main/scala/net/codingwell/weave/WeaveCompiler.scala
@@ -9,7 +9,7 @@ package net.codingwell.weave
 import com.google.inject._
 import scala.actors._
 import scala.collection.JavaConversions._
-import scala.collection.mutable.{Set => MutableSet}
+import scala.collection.mutable.{Set => MutableSet, Queue => MutableQueue}
 
 /*
 abstract class Executor {
@@ -19,12 +19,20 @@ abstract class Executor {
 
 object WeaveActor {
   case class QueueFile( file:WeaveFile )
+  case object Quit
 }
 
+import WeaveActor._
+
 class WeaveActor extends Actor {
+  val fileQueue = new MutableQueue[WeaveFile]
+  this.start
+
   def act() {
     loop {
       react {
+        case QueueFile(file) => fileQueue enqueue file
+        case Quit => exit
         case _ => println(this.toString() + " recieved unexpected message.")
       }
     }
@@ -33,10 +41,14 @@ class WeaveActor extends Actor {
 
 class WeaveCompiler @Inject() ( @Executor() val engines:java.util.Set[Actor] ) {
   
+  val weaveActor = new WeaveActor
+
   def compile( files:Seq[WeaveFile] ):Unit = {
     val nativeengines:MutableSet[Actor] = engines
     nativeengines foreach (_ ! "Message!")
     nativeengines foreach (_ ! "Message!")
     nativeengines foreach (_ ! 1)
+    Thread.sleep(3000)
+    weaveActor ! WeaveActor.Quit
   }
 }
